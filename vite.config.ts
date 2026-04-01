3581af88c39bc4a4aa1a6177ee3e81cf623fb70d
Ekansh Bajpai <120615669+ekansh-bajpai@users.noreply.github.com>
2026-04-01 13:40:56 +0200

    Set base path for deployment in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, ".", "");
   return {
     plugins: [react(), tailwindcss()],
+    base: '/ieee-summer-school/',
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "."),
