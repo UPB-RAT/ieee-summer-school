ad6a650455bd65b3ec1ad2c19cfcc631cd6cc896
Alper <alper.yegenoglu@rwth-aachen.de>
2026-04-07 15:06:11 +0200

    Change fetch URL for content.json
    Updated the fetch URL for content data as a backup solution. ATM commented out.

--- src/App.tsx
+++ src/App.tsx
@@ -1003,7 +1003,7 @@ export default function App() {
     const fetchData = async () => {
       try {
         const response = await fetch(
-          // "https://raw.githubusercontent.com/alperyeg/ieee-summer-school/refs/heads/main/public/data/content.json",
+          // "https://raw.githubusercontent.com/UPB-RAT/ieee-summer-school/refs/heads/main/public/data/content.json",
           "/ieee-summer-school/data/content.json",
         );
         if (!response.ok) {
