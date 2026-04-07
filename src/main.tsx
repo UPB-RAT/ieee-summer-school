116c5380422d9019882df79f41380faee78e8b99
Alper <alper.yegenoglu@rwth-aachen.de>
2026-04-07 18:00:58 +0200

    Disable Google Analytics initialization
    Comment out Google Analytics initialization code.

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,13 @@ import App from "./App.tsx";
 import "./index.css";
 import ReactGA from "react-ga4";
 
-const GA_ID = import.meta.env.VITE_GOOGLE_ANALYTICS_ID;
+// const GA_ID = import.meta.env.VITE_GOOGLE_ANALYTICS_ID;
 
-if (!GA_ID) {
-  console.warn("GA not configured");
-}
-// Initialize Google Analytics 4
-ReactGA.initialize(GA_ID ?? 'G-XXXXXXXXXX');
+// if (!GA_ID) {
+//   console.warn("GA not configured");
+// }
+// // Initialize Google Analytics 4
+// ReactGA.initialize(GA_ID ?? 'G-XXXXXXXXXX');
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
